2cade92c9d7344e3884821347eff24910ed1f612
Dom Barnett <dom.barnett@thetestpeople.com>
2014-10-16 15:29:36 +0100

    Adding smoke tests

---
+++ tests/pkg/__init__.py
@@ -0,0 +1,10 @@
+__author__ = 'Dom Barnett'
+
+from selenium import webdriver
+
+DRIVER = None
+
+def get_or_create_driver():
+    global DRIVER
+    DRIVER = DRIVER or webdriver.Firefox()
+    return DRIVER
