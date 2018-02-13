822d7265ecd0d80f26f6ade3c9bd819198355e39
George Hickman <george@ghickman.co.uk>
2018-02-13 14:45:26 +0000

    Exit when failing to cd to a directory

--- ansible/scripts/restore.sh
+++ ansible/scripts/restore.sh
@@ -13,7 +13,7 @@ USER_HOME=$(eval echo "~${SUDO_USER}")
 restore_dir="$USER_HOME/restored"
 
 # Download from S3
-cd "$USER_HOME"
+cd "$USER_HOME" || (echo "failed to cd into user home" && exit 1)
 python download.py "$1"
 echo "Downloaded backup"
 
