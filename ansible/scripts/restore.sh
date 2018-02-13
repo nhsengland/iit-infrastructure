10dcdafac313d2a19bdac6498eccfa533e48bb86
George Hickman <george@ghickman.co.uk>
2018-02-13 14:43:49 +0000

    Upgrade the db schema when restoring

--- ansible/scripts/restore.sh
+++ ansible/scripts/restore.sh
@@ -28,7 +28,10 @@ sudo service apache2 stop
 sudo service postgresql restart
 
 # Restore the databases.
+sudo /usr/lib/ckan/default/bin/paster --plugin=ckan db upgrade --config=/etc/ckan/default/production.ini
+
 sudo /usr/lib/ckan/default/bin/paster --plugin=ckan db clean -c /etc/ckan/default/production.ini
+
 # FIXME: generate a .dump file in the backup script
 sudo --user=postgres psql --quiet --dbname=ckan_default --file="$restore_dir/ckan_default.sql"
 # sudo --user=postgres pg_restore --dbname="ckan_default" --clean --if-exists "$restore_dir/ckan_default.dump"
