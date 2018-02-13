8d12b16764b57d830efb35f02cfb0cb57113d74e
George Hickman <george@ghickman.co.uk>
2018-02-13 14:44:13 +0000

    Add output to aid tracking down problems

--- ansible/scripts/restore.sh
+++ ansible/scripts/restore.sh
@@ -15,6 +15,7 @@ restore_dir="$USER_HOME/restored"
 # Download from S3
 cd "$USER_HOME"
 python download.py "$1"
+echo "Downloaded backup"
 
 # Untar/gunzip it
 rm -rf "$restore_dir"
@@ -22,29 +23,38 @@ mkdir "$restore_dir"
 mv "backup-$1.tgz" "$restore_dir/"
 
 tar xfz "$restore_dir/backup-$1.tgz" --directory="$restore_dir"
+echo "Extracted backup"
 
 # Kick some services in an appropriate way so restore isn't blocked.
 sudo service apache2 stop
+echo "Stopped Apache"
+
 sudo service postgresql restart
+echo "Restarted Postgres"
 
 # Restore the databases.
 sudo /usr/lib/ckan/default/bin/paster --plugin=ckan db upgrade --config=/etc/ckan/default/production.ini
+echo "Upgraded database schema"
 
 sudo /usr/lib/ckan/default/bin/paster --plugin=ckan db clean -c /etc/ckan/default/production.ini
 
 # FIXME: generate a .dump file in the backup script
 sudo --user=postgres psql --quiet --dbname=ckan_default --file="$restore_dir/ckan_default.sql"
+echo "Restored database: ckan_default"
 # sudo --user=postgres pg_restore --dbname="ckan_default" --clean --if-exists "$restore_dir/ckan_default.dump"
 sudo --user=postgres pg_restore --dbname="datastore_default" --clean --if-exists "$restore_dir/datastore_default.dump"
+echo "Restored database: datastore_default"
 
 # Copy the filestore directory into the backup directory.
 sudo cp -r "$restore_dir/default/"* /var/lib/ckan/default
 # Ensure that the restored directories are read/writable by Apache
 sudo chown -R www-data /var/lib/ckan/default
 sudo chgrp -R www-data /var/lib/ckan/default
+echo "Restored directory: /var/lib/ckan/default"
 
 # Restart Apache
 sudo service apache2 start
+echo "Started Apache"
 
 # Reindex SOLR:
 /usr/lib/ckan/default/bin/paster --plugin=ckan user list -c /etc/ckan/default/production.ini search-index rebuild
